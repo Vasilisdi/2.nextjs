94ca88d419bf30de2d753ce447e84c9ea412b95f
Vasileios Dimitriou <dimitriouavasileios@gmail.com>
2025-03-21 23:56:52 +0200

    Communication with database trial 8

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAdmin = createClient(
-    process.env.SUPABASE_URL!,  // Server-side environment variable
-    process.env.SUPABASE_SERVICE_ROLE_KEY!  // Server-side service role key
+    process.env.NEXT_PUBLIC_SUPABASE_URL!, // Supabase URL
+    process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY! // Service role key
 );
